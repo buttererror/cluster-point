be6fbb111c3c4c9c0913c9a43efa64cb99469224
Mahmoud Ahmed <buttererror@gmail.com>
2022-04-07 23:44:34 +0200

    put out the layout

---
+++ src/components/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="layout">
+    <sidebar></sidebar>
+    <map></map>
+  </div>
+</template>
+
+<script>
+import Sidebar from "./Sidebar";
+export default {
+  name: "Layout",
+  components: {Sidebar}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="sidebar"></div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+}
+</script>
+
+<style scoped>
+
+</style>
